c6414656a2fe3c3f3e0cfb59fac8df229002a2d9
Alejandro <rodriguezalvarez@gmail.com>
2023-05-17 17:12:39 +0200

    Documentacion de funciones

--- main.sh
+++ main.sh
@@ -3,15 +3,41 @@
 # Array of arguments
 arg=($@)
 
-# Muestra la lista de panes con la informacion necesaria
 
 function list () {
+: << DOC
+Descripcion:
+    Muestra la lista de TODOS los panes activos.
+Tipo:
+    Funcion
+Parametros:
+    Ninguno
+Salida:
+    Stdout de:
+      - Nombre de la session
+      - Numero de ventanas
+      - Id de la ventana.
+      - Numero de panes.
+      - Window Layout.
+      - Path de los panes.
+DOC
     TAB=$'\t'
     NL=$'\n'
     tmux list-panes -a -F "#S${TAB}#{session_windows}${TAB}#{window_id}${TAB}#{window_panes}${TAB}#{window_layout}${TAB}#{pane_current_path}${TAB}"
 }
 
 function numSession () {
+: << DOC
+Descripcion:
+    Devuelve el numero de sessiones activas.
+Tipo:
+    Funcion
+Parametros:
+    Ninguno
+Salida:
+   Stdout de numero.
+
+DOC
     i=0
     for x in $(tmux list-sessions -F "#S");
     do
@@ -20,47 +46,76 @@ function numSession () {
     echo $i
 }
 
-#function Prueba {
-    #nSession=$(numSession)
-    #echo "El numero de Sessions es: $nSession"
-#}
-
 function nameSession () {
+: << DOC
+Descripcion:
+    Devuelve "arreglo" con los nombre de las sessiones activas
+Tipo:
+    Funcion
+Parametros:
+    Ninguno
+Salida:
+    Stdout del array con los nombres.
+DOC
+
     arr=()
-    for session in $(tmux list-sessions -F "#S");
-    do
+    for session in $(tmux list-sessions -F "#S");do
         arr+=("session:$session ")
     done
 
-    printf "%s" "${arr[@]@Q}"
+    printf "%s" "${arr[@]}"
 }
 
 
 function infoWindow () {
+: << DOC
+Descripcion:
+Tipo:
+Parametros:
+Salida:
+
+DOC
 
     TAB=$'\t'
     arr=()
 
     IFS=':' read -a arr <<< $(tmux list-windows -t $1 -f "#{==:#{window_index},$2}" -F "#{window_index}:#W:#{window_layout}:#{window_panes}")
 
-    echo "${arr[@]@Q}"
+    echo "${arr[@]}"
 }
 
 
 function PruebaInfoWindow {
+: << DOC
+Descripcion:
+Tipo:
+Parametros:
+Salida:
+
+DOC
     infoWindow TMUX 0
 }
 
 
 
-function createYAML {
+function createYAML () {
+: << DOC
+Descripcion:
+    Funcion que busca las sessiones abierta, las compara con las guardadas y genera un archivo YAML para guardar los cambios.
+Tipo:
+    Funcion
+Parametros:
+    Ninguno
+Salida:
+    Stdout del archivo YAML
+DOC
+
     /bin/cat<<YAML
 ---
 YAML
 
     for x in $(nameSession);do
         session=$(echo "$x" | cut -d ":" -f 2)
-        echo $session
         session=$(sessionExit $session) #Verifica que la session exite
         if [ -z $session ];then
             echo "Entonces la crea"
@@ -99,11 +154,25 @@ YAML
 
 
 function saveYAML {
+: << DOC
+Descripcion:
+Tipo:
+Parametros:
+Salida:
+
+DOC
     createYAML > ~/.tmux-session
 }
 
 
 function sessionExit {
+: << DOC
+Descripcion:
+Tipo:
+Parametros:
+Salida:
+
+DOC
     #var="session:${arg[1]}"
     var="session:$1"
 
@@ -123,6 +192,13 @@ function sessionExit {
 
 
 function Prueba {
+: << DOC
+Descripcion:
+Tipo:
+Parametros:
+Salida:
+
+DOC
     arr=$(tmux list-sessions -F "#S" )
     arr=(${arr[@]} "100" "200")
 
